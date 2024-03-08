03c6624b50eb2edcc49b4b6e1207aa84615db260
Danis Kurenkiev <kurenkievdd@gmail.com>
2024-03-08 17:36:51 +0600

    Deleted extra code

--- src/main/java/kg/attractor/jobsearch/JobSearchApplication.java
+++ src/main/java/kg/attractor/jobsearch/JobSearchApplication.java
@@ -1,7 +1,5 @@
 package kg.attractor.jobsearch;
 
-import kg.attractor.jobsearch.model.Category;
-import kg.attractor.jobsearch.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
