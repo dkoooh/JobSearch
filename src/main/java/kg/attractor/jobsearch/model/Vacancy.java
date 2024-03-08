2a1e188ab3a9b52b7c55041f9894d4f7f341ae5f
Danis Kurenkiev <kurenkievdd@gmail.com>
2024-03-08 17:03:59 +0600

    Added vacancy fields

--- src/main/java/kg/attractor/jobsearch/model/Vacancy.java
+++ src/main/java/kg/attractor/jobsearch/model/Vacancy.java
@@ -1,5 +1,22 @@
 package kg.attractor.jobsearch.model;
 
-public class Vacancy {
+import lombok.Builder;
+import lombok.Data;
 
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class Vacancy {
+    private Integer id;
+    private String name;
+    private String description;
+    private Integer category_id;
+    private Double salary;
+    private Integer expFrom;
+    private Integer expTo;
+    private Boolean isActive;
+    private Integer authorId;
+    private LocalDate createdTime;
+    private LocalDate updatedTime;
 }
