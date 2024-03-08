87fa03b009ba96eeb3c415ccaa632003119ba8a8
Danis Kurenkiev <kurenkievdd@gmail.com>
2024-03-08 16:47:24 +0600

    Added user fields

--- src/main/java/kg/attractor/jobsearch/JobSearchApplication.java
+++ src/main/java/kg/attractor/jobsearch/JobSearchApplication.java
@@ -1,5 +1,6 @@
 package kg.attractor.jobsearch;
 
+import kg.attractor.jobsearch.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/kg/attractor/jobsearch/model/User.java
+++ src/main/java/kg/attractor/jobsearch/model/User.java
@@ -1,7 +1,19 @@
 package kg.attractor.jobsearch.model;
 
 import kg.attractor.jobsearch.enums.AccountType;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class User {
-
+    private Integer id;
+    private String name;
+    private String surname;
+    private Integer age;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String avatar;
+    private AccountType accountType;
 }
